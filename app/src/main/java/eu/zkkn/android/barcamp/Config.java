48e1c9e74076521a8ac342e3609af2adf66c6790
Zdeněk Kořán <zkoran@gmail.com>
2015-03-31 13:29:50 +0200

    add simple request to JSON API
    replace text in TextView with string from API

---
+++ app/src/main/java/eu/zkkn/android/barcamp/Config.java
@@ -0,0 +1,23 @@
+package eu.zkkn.android.barcamp;
+
+/**
+ * App Configuration
+ */
+public class Config {
+
+    /**
+     * Enable / Disable debug messages
+     */
+    public static final boolean DEBUG = true;
+
+    /**
+     * Debug log tag
+     */
+    public static final String TAG = "BarcampLog";
+
+    /**
+     * API URL
+     */
+    public static final String API_URL = "http://demo5483269.mockable.io/";
+
+}
--- app/src/main/java/eu/zkkn/android/barcamp/MainActivity.java
+++ app/src/main/java/eu/zkkn/android/barcamp/MainActivity.java
@@ -1,19 +1,67 @@
 package eu.zkkn.android.barcamp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mName = (TextView) findViewById(R.id.tv_name);
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        JsonObjectRequest request = new JsonObjectRequest(Config.API_URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            mName.setText(response.getString("name"));
+                        } catch (JSONException e) {
+                            if (Config.DEBUG) Log.d(Config.TAG, e.getMessage());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (Config.DEBUG) Log.d(Config.TAG, error.getMessage());
+                    }
+                }
+        );
+
+        request.setTag(this);
+
+        VolleySingleton.getInstance(this).addToRequestQueue(request);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        VolleySingleton.getInstance(this).getRequestQueue().cancelAll(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
